3ca1653dce8f4c659dbe77f22f8c3a39fffb4ba1
Danuk <danuk77@live.co.uk>
2023-09-05 15:55:05 +0100

    Added new activity for editing the url, title and description before sending to notion

--- app/src/main/java/com/example/netnook_android/MainActivity.java
+++ app/src/main/java/com/example/netnook_android/MainActivity.java
@@ -32,7 +32,7 @@ import androidx.security.crypto.EncryptedSharedPreferences;
 import androidx.security.crypto.MasterKey;
 
 public class MainActivity extends AppCompatActivity {
-    private static String key = "dfnvwuvh02vj-293vfkjwgg23vb23vfjhwbvyHbpqjOvwvacavk''o1cnajcii";
+    private static final String key = "dfnvwuvh02vj-293vfkjwgg23vb23vfjhwbvyHbpqjOvwvacavk''o1cnajcii";
 
     private EditText integrationKeyField;
     private EditText databaseIDField;
@@ -45,8 +45,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Handle the intent sent by the user
-        handleIntent(getIntent());
 
         // Get references to the two EditText fields
         integrationKeyField = findViewById(R.id.integrationKeyInput);
@@ -59,15 +57,28 @@ public class MainActivity extends AppCompatActivity {
         if(checkKeys(this)){
             integrationKeyField.setText(this.integrationKey);
             databaseIDField.setText(this.databaseId);
+            // Handle the intent sent by the user
+            handleIntent(getIntent());
+        }else{
+            Toast.makeText(this,"Please ensure that the configuration is correctly entered",3);
         }
-
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        // Handle the intent sent by the user
-        handleIntent(intent);
+
+
+        // Check if the user has already configured the keys, if so populate the above two Views with that information
+        // Otherwise the function informs the user to enter the keys
+        if(checkKeys(this)){
+            integrationKeyField.setText(this.integrationKey);
+            databaseIDField.setText(this.databaseId);
+            // Handle the intent sent by the user
+            handleIntent(intent);
+        }else{
+            Toast.makeText(this,"Please ensure that the configuration is correctly entered",3);
+        }
     }
 
     /**
@@ -90,115 +101,58 @@ public class MainActivity extends AppCompatActivity {
 
                     // Construct the request body (JSON object) being sent to the server
                     try{
-                        // Send the information to notion
-                        sendToNotion(url, title, "fb62e501905e400c988b713255375537", "Testing android version", "secret_Ch48QmwyMenBcDyoIV96xTA4I5WaSy2vlvJMfBhRgEZ");
+                        Intent i = new Intent(this, createNewBookmark.class);
+                        // Pass the information into the new page
+                        i.putExtra("url", url);
+                        i.putExtra("title", title);
+                        i.putExtra("integrationKey", this.integrationKey);
+                        i.putExtra("databaseID", this.databaseId);
+                        startActivity(i);
                     } catch (Exception e) {
                         Log.e("userMsg", "Error occurred");
                     }
+                }else{
+                    Toast.makeText(this,"Unsopported app", Toast.LENGTH_SHORT);
                 }
             }
         }
     }
 
     /**
-     * Function used for creating a request to the Notion API to create an entry for the given database ID.
-     * @param url URL to be stored
-     * @param title Title to be stored
-     * @param dbID  Database ID inside of notion to store the new entry
-     * @param desc  Description to be stored
-     * @param intKey Integration key of the user
+     * Method called at the beginning of the application start to populate the integration key and the database ID fields with the information the user had previously entered.
+     * If the keys are in the store, it poluates the two fields in the UI
+     * @param context Context
+     * @return Returns true if the keys were stored, false otherwise
      */
-    protected void sendToNotion(String url, String title, String dbID, String desc, String intKey){
-        RequestQueue volleyQueue = Volley.newRequestQueue(MainActivity.this);
-        // Notion API endpoint
-        String fetchUrl = "https://api.notion.com/v1/pages";
+    private boolean checkKeys(Context context){
 
-        // JSON object to send
-        JSONObject requestBody = new JSONObject();
+        try{
+            // Either builds or gets the encryption key
+            MasterKey masterKey = new MasterKey.Builder(context)
+                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                    .build();
 
-        // Construct the body of the request
-        try {
+            SharedPreferences sharedPreferences = EncryptedSharedPreferences.create(
+                    context,
+                    key,
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
 
-            // Database info
-            JSONObject parent = new JSONObject();
-            parent.put("type", "database_id");
-            parent.put("database_id", dbID);
-
-            // Properties field
-            JSONObject textTitle = new JSONObject();
-            textTitle.put("content", title);
-            JSONArray titleArray = new JSONArray();
-            JSONObject text = new JSONObject();
-            text.put("text", textTitle);
-            titleArray.put(text);
-            JSONObject TitleObject = new JSONObject();
-            TitleObject.put("title", titleArray);
-
-            JSONObject link = new JSONObject();
-            link.put("url", url);
-
-            JSONObject textDescription = new JSONObject();
-            textDescription.put("content", desc);
-            JSONArray richText = new JSONArray();
-            JSONObject rich = new JSONObject();
-            rich.put("text", textDescription);
-            richText.put(rich);
-            JSONObject description = new JSONObject();
-            description.put("rich_text",richText);
-
-            JSONObject properties = new JSONObject();
-            properties.put("Title", TitleObject);
-            properties.put("Link", link);
-            properties.put("Description", description);
-
-            // Connect all objects
-            requestBody.put("parent", parent);
-            requestBody.put("properties", properties);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
+            if(sharedPreferences.contains("intKey") &&  sharedPreferences.contains("dbID")){
+                this.integrationKey = sharedPreferences.getString("intKey", "Invalid");
+                this.databaseId = sharedPreferences.getString("dbID", "Invalid");
+                return true;
+            }else{
+                // Alert the user to make sure that their information is properly entered
+                Toast.makeText(this, "Please ensure both the integration and datbase keys are entered", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }catch(Exception e){
+            Log.v("userMsg", e.toString());
         }
-
-        StringRequest request = new StringRequest(
-                Request.Method.POST,
-                fetchUrl,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.v("userMsg", "Success");
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.v("userMsg", "Failure");
-                    }
-                }
-        ) {
-            @Override
-            public byte[] getBody() {
-                // Convert the JSON request body to bytes.
-                return requestBody.toString().getBytes();
-            }
-
-            @Override
-            public String getBodyContentType() {
-                // Set the content type for the request body.
-                return "application/json";
-            }
-
-            @Override
-            public Map<String, String> getHeaders() {
-                // Set the required headers, including the Authorization header.
-                Map<String, String> headers = new HashMap<>();
-                headers.put("Authorization", "Bearer secret_Ch48QmwyMenBcDyoIV96xTA4I5WaSy2vlvJMfBhRgEZ");
-                headers.put("Notion-Version", "2022-06-28");
-                return headers;
-            }
-        };
-
-        // Make the request
-        volleyQueue.add(request);
+        return false;
     }
 
     /**
@@ -244,46 +198,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Wrapper function to link the button click and the save keys
+     * Wrapper method for calling the saveKeys method
+     * @param v Reference to the button view
      */
     public void saveWrapper(View v){
-        saveKeys(this);
-    }
-
-    /**
-     * Method called at the beginning of the application start to populate the integration key and the database ID fields with the information the user had previously entered.
-     * If the keys are in the store, it poluates the two fields in the UI
-     * @param context Context
-     * @return Returns true if the keys were stored, false otherwise
-     */
-    private boolean checkKeys(Context context){
-
-        try{
-            // Either builds or gets the encryption key
-            MasterKey masterKey = new MasterKey.Builder(context)
-                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-                    .build();
-
-            SharedPreferences sharedPreferences = EncryptedSharedPreferences.create(
-                    context,
-                    key,
-                    masterKey,
-                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-            );
-
-            if(sharedPreferences.contains("intKey") &&  sharedPreferences.contains("dbID")){
-                this.integrationKey = sharedPreferences.getString("intKey", "Invalid");
-                this.databaseId = sharedPreferences.getString("dbID", "Invalid");
-                return true;
-            }else{
-                // Alert the user to make sure that their information is properly entered
-                Toast.makeText(this, "Please ensure both the integration and datbase keys are entered", Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        }catch(Exception e){
-            Log.v("userMsg", e.toString());
+        if(saveKeys(this)){
+            Log.v("userMsg", "Success");
         }
-        return false;
     }
 }
---
+++ app/src/main/java/com/example/netnook_android/createNewBookmark.java
@@ -0,0 +1,163 @@
+package com.example.netnook_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKey;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class createNewBookmark extends AppCompatActivity {
+
+    private EditText url;
+    private EditText title;
+    private EditText description;
+    private static String integrationKey;
+    private static String databaseID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_bookmark);
+
+        // Instantiate the private properties
+        url = findViewById(R.id.urlInput);
+        title = findViewById(R.id.titleInput);
+        description = findViewById(R.id.descriptionInput);
+
+        // Populate the fields and get the configuration
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        integrationKey = b.getString("integrationKey");
+        databaseID = b.getString("databaseID");
+        url.setText(b.getString("url"));
+        title.setText(b.getString("title"));
+    }
+
+    /**
+     * Method called when the button is clicked to finally save the bookmark in Notion
+     * @param v Reference to the button object
+     */
+    public void submit(View v){
+        // Submit it to notion
+        sendToNotion(this.integrationKey, description.getText().toString());
+    }
+
+    /**
+     * @param intKey Notion integration key
+     * Function used for creating a request to the Notion API to create an entry for the given database ID.
+     */
+    protected void sendToNotion(String intKey, String desc){
+        RequestQueue volleyQueue = Volley.newRequestQueue(this);
+        // Notion API endpoint
+        String fetchUrl = "https://api.notion.com/v1/pages";
+
+        // JSON object to send
+        JSONObject requestBody = new JSONObject();
+
+        // Construct the body of the request
+        try {
+
+            // Database info
+            JSONObject parent = new JSONObject();
+            parent.put("type", "database_id");
+            parent.put("database_id", this.databaseID);
+
+            // Properties field
+            JSONObject textTitle = new JSONObject();
+            textTitle.put("content", this.title.getText());
+            JSONArray titleArray = new JSONArray();
+            JSONObject text = new JSONObject();
+            text.put("text", textTitle);
+            titleArray.put(text);
+            JSONObject TitleObject = new JSONObject();
+            TitleObject.put("title", titleArray);
+
+            JSONObject link = new JSONObject();
+            link.put("url", this.url.getText());
+
+            JSONObject textDescription = new JSONObject();
+            textDescription.put("content", desc);
+            JSONArray richText = new JSONArray();
+            JSONObject rich = new JSONObject();
+            rich.put("text", textDescription);
+            richText.put(rich);
+            JSONObject description = new JSONObject();
+            description.put("rich_text",richText);
+
+            JSONObject properties = new JSONObject();
+            properties.put("Title", TitleObject);
+            properties.put("Link", link);
+            properties.put("Description", description);
+
+            // Connect all objects
+            requestBody.put("parent", parent);
+            requestBody.put("properties", properties);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                fetchUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.v("userMsg", "Success");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.v("userMsg", "Failure");
+                    }
+                }
+        ) {
+            @Override
+            public byte[] getBody() {
+                // Convert the JSON request body to bytes.
+                return requestBody.toString().getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                // Set the content type for the request body.
+                return "application/json";
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+                // Set the required headers, including the Authorization header.
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + intKey);
+                headers.put("Notion-Version", "2022-06-28");
+                return headers;
+            }
+        };
+
+        // Make the request
+        volleyQueue.add(request);
+    }
+
+}
