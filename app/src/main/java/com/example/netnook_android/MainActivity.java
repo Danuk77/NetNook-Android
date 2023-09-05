81097a6bfe451121c02b8048573937400e321426
Danuk <danuk77@live.co.uk>
2023-09-05 01:52:35 +0100

    Added functionality to store the user's notion keys in EncryptedSharedPreferences and added the behaviour to automatically populate the input fields with the keys when the user opens the app

--- app/src/main/java/com/example/netnook_android/MainActivity.java
+++ app/src/main/java/com/example/netnook_android/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.netnook_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.ClientError;
@@ -23,13 +27,40 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.content.Context;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKey;
+
 public class MainActivity extends AppCompatActivity {
+    private static String key = "dfnvwuvh02vj-293vfkjwgg23vb23vfjhwbvyHbpqjOvwvacavk''o1cnajcii";
+
+    private EditText integrationKeyField;
+    private EditText databaseIDField;
+    private Button submit;
+
+    private String integrationKey;
+    private String databaseId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Handle the intent sent by the user
         handleIntent(getIntent());
+
+        // Get references to the two EditText fields
+        integrationKeyField = findViewById(R.id.integrationKeyInput);
+        databaseIDField = findViewById(R.id.databaseIDInput);
+        submit = findViewById(R.id.submitButton);
+
+
+        // Check if the user has already configured the keys, if so populate the above two Views with that information
+        // Otherwise the function informs the user to enter the keys
+        if(checkKeys(this)){
+            integrationKeyField.setText(this.integrationKey);
+            databaseIDField.setText(this.databaseId);
+        }
+
     }
 
     @Override
@@ -69,8 +100,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Function used for creating a request to the Notion API to create an entry for the given database ID.
+     * @param url URL to be stored
+     * @param title Title to be stored
+     * @param dbID  Database ID inside of notion to store the new entry
+     * @param desc  Description to be stored
+     * @param intKey Integration key of the user
+     */
     protected void sendToNotion(String url, String title, String dbID, String desc, String intKey){
         RequestQueue volleyQueue = Volley.newRequestQueue(MainActivity.this);
+        // Notion API endpoint
         String fetchUrl = "https://api.notion.com/v1/pages";
 
         // JSON object to send
@@ -111,6 +151,7 @@ public class MainActivity extends AppCompatActivity {
             properties.put("Link", link);
             properties.put("Description", description);
 
+            // Connect all objects
             requestBody.put("parent", parent);
             requestBody.put("properties", properties);
 
@@ -158,5 +199,91 @@ public class MainActivity extends AppCompatActivity {
 
         // Make the request
         volleyQueue.add(request);
+    }
+
+    /**
+     * Method to store the integration and database keys in encrypted shared preferences
+     * @param context Context
+     * @return
+     */
+    private boolean saveKeys(Context context){
+        try{
+            String intKey = this.integrationKeyField.getText().toString();
+            String dbID = this.databaseIDField.getText().toString();
+
+            // Check if the two fields are empty or not
+            if(intKey.isEmpty() || dbID.isEmpty()){
+                Toast.makeText(this, "Please enter values for both the fields", Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+
+            // Either builds or gets the encryption key
+            MasterKey masterKey = new MasterKey.Builder(context)
+                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                    .build();
+
+            SharedPreferences sharedPreferences = EncryptedSharedPreferences.create(
+                    context,
+                    key,
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+
+            // Save the integration key and the database id
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("intKey", intKey);
+            editor.putString("dbID", dbID);
+            editor.apply();
+        }catch (Exception e){
+            Log.v("userMsg", e.toString());
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Wrapper function to link the button click and the save keys
+     */
+    public void saveWrapper(View v){
+        saveKeys(this);
+    }
+
+    /**
+     * Method called at the beginning of the application start to populate the integration key and the database ID fields with the information the user had previously entered.
+     * If the keys are in the store, it poluates the two fields in the UI
+     * @param context Context
+     * @return Returns true if the keys were stored, false otherwise
+     */
+    private boolean checkKeys(Context context){
+
+        try{
+            // Either builds or gets the encryption key
+            MasterKey masterKey = new MasterKey.Builder(context)
+                    .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                    .build();
+
+            SharedPreferences sharedPreferences = EncryptedSharedPreferences.create(
+                    context,
+                    key,
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+
+            if(sharedPreferences.contains("intKey") &&  sharedPreferences.contains("dbID")){
+                this.integrationKey = sharedPreferences.getString("intKey", "Invalid");
+                this.databaseId = sharedPreferences.getString("dbID", "Invalid");
+                return true;
+            }else{
+                // Alert the user to make sure that their information is properly entered
+                Toast.makeText(this, "Please ensure both the integration and datbase keys are entered", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }catch(Exception e){
+            Log.v("userMsg", e.toString());
+        }
+        return false;
     }
 }
